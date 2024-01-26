0a95c852bbec6ed97f0e5762d6740f15bd3b0ae3
Mona Hewidy <monaahmedhewidy@gmail.com>
2024-01-26 16:19:34 +0200

    feat: add cell fill

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,17 @@
 export default function Home() {
   return (
-    <main>
-      <h1>test</h1>
-    </main>
+    <div className="container">
+      <div className="frame">
+        <div>1</div>
+        <div>2</div>
+        <div>3</div>
+        <div>4</div>
+        <div>5</div>
+        <div>6</div>
+        <div>7</div>
+        <div>8</div>
+        <div>9</div>
+      </div>
+    </div>
   );
 }
