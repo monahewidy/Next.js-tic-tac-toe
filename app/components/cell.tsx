0f0c68b409861e879ceee9423b3440fcb7a792e9
Mona Hewidy <monaahmedhewidy@gmail.com>
2024-01-26 19:25:46 +0200

    feat: add x, o to the table

--- app/components/cell.tsx
+++ app/components/cell.tsx
@@ -1,4 +1,36 @@
-const Cell = () => {
-  return <div className="box"></div>;
+import { Dispatch, SetStateAction } from "react";
+
+type CellProps = {
+  id: number;
+  go: string;
+  setGo: Dispatch<SetStateAction<string>>;
+  cells: string[];
+  setCells: Dispatch<SetStateAction<string[]>>;
+  cell: string;
+};
+const Cell = ({ go, setGo, id, cells, setCells, cell }: CellProps) => {
+  const handleClick = (e) => {
+    const notTaken = !cells[id];
+    if (notTaken) {
+      if (go === "circle") {
+        handleCellChange("circle");
+        setGo("cross");
+      } else if (go === "cross") {
+        handleCellChange("cross");
+        setGo("circle");
+      }
+    }
+  };
+
+  const handleCellChange = (cellToChange: string) => {
+    let copyCells = [...cells];
+    copyCells[id] = cellToChange;
+    setCells(copyCells);
+  };
+  return (
+    <div className="box" onClick={handleClick}>
+      <div className={cell}>{cell ? (cell === "circle" ? "0" : "X") : ""}</div>
+    </div>
+  );
 };
 export default Cell;
--- app/page.tsx
+++ app/page.tsx
@@ -4,11 +4,22 @@ import Cell from "./components/cell";
 
 export default function Home() {
   const [cells, setCells] = useState(["", "", "", "", "", "", "", "", ""]);
+  const [go, setGo] = useState("circle");
+
+  console.log(cells);
   return (
     <div className="container">
       <div className="gameFrame">
         {cells.map((cell, index) => (
-          <Cell key={index} />
+          <Cell
+            key={index}
+            go={go}
+            setGo={setGo}
+            id={index}
+            cells={cells}
+            setCells={setCells}
+            cell={cell}
+          />
         ))}
       </div>
     </div>
