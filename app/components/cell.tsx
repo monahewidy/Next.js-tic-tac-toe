152496f6e5bad12fc898fa0d002c47825cc265c8
Mona Hewidy <monaahmedhewidy@gmail.com>
2024-01-26 16:52:48 +0200

    style: add style for cells

---
+++ app/components/cell.tsx
@@ -0,0 +1,4 @@
+const Cell = () => {
+  return <div className="box"></div>;
+};
+export default Cell;
--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,15 @@
+"use client";
+import { useState } from "react";
+import Cell from "./components/cell";
+
 export default function Home() {
+  const [cells, setCells] = useState(["", "", "", "", "", "", "", "", ""]);
   return (
     <div className="container">
-      <div className="frame">
-        <div>1</div>
-        <div>2</div>
-        <div>3</div>
-        <div>4</div>
-        <div>5</div>
-        <div>6</div>
-        <div>7</div>
-        <div>8</div>
-        <div>9</div>
+      <div className="gameFrame">
+        {cells.map((cell, index) => (
+          <Cell key={index} />
+        ))}
       </div>
     </div>
   );
